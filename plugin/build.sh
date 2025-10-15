b00c50aaeabe55013031f660946645d6eaf984d6
Amy Parent <amy@amyparent.com>
2025-10-15 21:53:14 +0100

    Improve macOS build system

--- plugin/build.sh
+++ plugin/build.sh
@@ -116,7 +116,67 @@ if [ -z "$OPENGPWS" ]; then
 	exit 1
 fi
 
-set -e
+# set -e
+
+function sign {
+	TARGET="$1"
+	PLUGIN="$TARGET/mac_x64/$TARGET.xpl"
+	BUNDLE_ID="skiselkov.libradio"
+	
+	CERT=$(security find-certificate -Z -c "Developer ID Application:" | \
+		grep "SHA-1" | \
+		awk 'NF { print $NF }')
+	
+	echo "Signing $PLUGIN ($BUNDLE_ID)..."
+	codesign "$PLUGIN" \
+		--sign "$CERT" \
+		--force \
+		--options runtime \
+		--timestamp \
+		--identifier "$BUNDLE_ID"
+}
+
+
+function notarize {
+	TARGET="$1"
+	PLUGIN="$1/mac_x64/$1.xpl"
+	ZIPFILE="$(mktemp -u /tmp/$TARGET.XXXXXX).zip"
+	PLISTBUDDY="/usr/libexec/PlistBuddy"
+	
+	SUBMIT_LOG="notarize/submission.plist"
+	RESULT_LOG="notarize/notarization.plist"
+	
+	echo "Creating /notarize..."
+	mkdir -p notarize
+	
+	echo "Packaging $PLUGIN for notarization..."
+	zip "$ZIPFILE" "$PLUGIN" > /dev/null
+	
+	echo "Submitting notarization request..."
+	xcrun notarytool submit "$ZIPFILE" \
+		--keychain-profile AC_PASSWORD \
+		--output-format plist > "$SUBMIT_LOG" || exit 1
+	ID=$($PLISTBUDDY -c "Print :id" "$SUBMIT_LOG")
+	
+	xcrun notarytool wait "$ID" \
+		--keychain-profile AC_PASSWORD || exit 1
+	
+	echo "Notarization complete"
+	xcrun notarytool info "$ID" \
+		--keychain-profile AC_PASSWORD \
+		--output-format plist > "$RESULT_LOG" || exit 1
+	
+	STATUS=$($PLISTBUDDY -c "Print :status" "$RESULT_LOG")
+	
+	if [[ "$STATUS" == "Accepted" ]]; then
+		echo "Notarization successful"
+	else
+		echo "Notarization error\nCheck notarize/notarization.plist for more info"
+	fi
+	
+	rm "$ZIPFILE"
+}
+
 
 case "$(uname)" in
 Darwin)
@@ -126,9 +186,10 @@ Darwin)
 		cmake . -DOPENGPWS="$OPENGPWS" -DLIBACFUTILS="$LIBACFUTILS" \
 		    $CMAKE_OPTS
 		cmake --build . --parallel "$NCPUS"
-		if [ -n "$NOTARIZE" ]; then
-			make -f notarize/notarize.make notarize
-		fi
+	fi
+	if [ -n "$NOTARIZE" ]; then
+		sign "libradio.plugin"
+		notarize "libradio.plugin"
 	fi
 	;;
 Linux)
